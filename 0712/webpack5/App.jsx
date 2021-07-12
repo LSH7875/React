4001821350a71d9c264f1847ae63888d8a25ef90
LSH7875 <tmdgml7875@gmail.com>
2021-07-12 17:36:41 +0900

    오늘 수업 내용

---
+++ 0712/webpack5/App.jsx
@@ -0,0 +1,19 @@
+import React , {Component} from 'react'
+import Memo from '../webpack5/components/memo'
+
+
+
+
+
+
+
+
+
+const App=()=>{
+    return(
+        <><Memo/>
+        </>
+    )
+}
+
+export default App 
